8988d40c3ff8ccd18a8345fa52fd067c2a99c01e
lowenchi0509 <58016152+lowenchi0509@users.noreply.github.com>
2020-05-17 09:19:02 +0800

    Update settings.py

--- linebotHotel/settings.py
+++ linebotHotel/settings.py
@@ -15,6 +15,13 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+from linebot import (
+    LineBotApi, WebhookHandler
+)
+from linebot.exceptions import (
+    InvalidSignatureError
+)
+from linebot.models import *
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
